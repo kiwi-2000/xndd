01972199c8e42880cfb0049a7e1f93280f707f8a
kiwi-2000 <77914783+kiwi-2000@users.noreply.github.com>
2021-01-24 21:25:23 +0800

    Update first.vue
    gai2

--- first.vue
+++ first.vue
@@ -1,18 +1,18 @@
 {obj}JSON:{
   [Array] message:{
-   {obj} 0{
+   {obj} 0:{
      "String" image_src: https://i.loli.net/2021/01/22/RB2likqYgJQAvjN.jpg
      "String" open_type: navigate
       Number goods_id: 129
      "String" navigator_url: /pages/goods-detail/goods-detail?goods_id=129
    }
-   {obj} 1{
+   {obj} 1:{
      "String" image_src: https://i.loli.net/2021/01/24/XdIz24mBieSfJDo.jpg
      "String" open_type: navigate
       Number goods_id: 129
      "String" navigator_url: /pages/goods-detail/goods-detail?goods_id=129
    }
-   {obj} 2{
+   {obj} 2:{
      "String" image_src: https://i.loli.net/2021/01/24/MvtKdoSZbwIeCmy.jpg
      "String" open_type: navigate
       Number goods_id: 129
